c440b7d4cfb6fe03795de145fc845a5f0d99e855
Natalie Vu <tnvu7@myseneca.ca>
2020-11-10 15:21:18 -0500

    database

--- data-service.js
+++ data-service.js
@@ -1,3 +1,22 @@
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize("d3uecanueahl90", "ewbtphjmloxgjl", "11919b99feac6084af2f87917e45373f1c6a6dfc29b976141f7e59cf15c976cc", 
+{
+    host: "ec2-34-232-24-202.compute-1.amazonaws.com",
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+});
+
+sequelize.authenticate().then(()=> console.log('Connection success.'))
+.catch((err) => console.log("Unable to connect to DB.", err));
+
+
 exports.initialize=()=>{
     return new Promise(function(resolve, reject) {
         reject();
